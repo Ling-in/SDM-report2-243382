d579289d74158e54115582bcfa3269a44cb2d28d
Ling-in <lingonjade@gmail.com>
2025-01-30 11:27:42 +0900

    calc

--- calc_mul.py
+++ calc_mul.py
@@ -5,11 +5,11 @@ import re
 def calc(A,B):
         ai=str(A)
         bi=str(B)
-        p = re.compile('\d+(\.\d+)?')
-        if p.match(ai) or p.match(bi):
+        p = re.compile('^[1-9][0-9]*$')#文字列が正の整数のみ
+        if p.match(ai) and p.match(bi):#両方が条件を満たすように変更
                 a=float(ai)
                 b=float(bi)
-                if 0<a and a<b and b<1000:
+                if 1<=a and a<=999 and 1<=b and b<=999:#a,bがそれぞれ1~999の範囲内に収まるように変更
                         valid=True
                 else:
                         valid=False
